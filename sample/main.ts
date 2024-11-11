35031456afa3332199f121311c9034863a0adeb3
namphan22 <phanvanhoainam22@gmail.com>
2024-11-12 00:08:25 +0700

    chore: update  intergration example

--- sample/main.ts
+++ sample/main.ts
@@ -1,22 +1,30 @@
+import 'dotenv/config';
+import { OauthProviderFactory, OauthSocialProvider } from 'fusion-auth';
 
-import { OauthProviderFactory, OauthSocialProvider } from "fusion-auth"
-async function testOauthWithGoogle() {
-  const userInfo = await OauthProviderFactory.createProvider(
-    {
-      clientId:
-        "your app clientId",
-      clientSecret: "your app clientSecret",
-      redirectUri: "your app redirectUri",
-    },
-    OauthSocialProvider.GOOGLE
-  ).verifyCode(
-    "responseCode"
-  );
+async function getGoogleUserInfo(responseCode: string) {
+    const oauthConfig = {
+        clientId: process.env.CLIENT_ID || '',
+        clientSecret: process.env.CLIENT_SECRET || '',
+        redirectUri: process.env.REDIRECT_URI || '',
+    };
 
-  console.log({ userInfo });
+    try {
+        const googleProvider = OauthProviderFactory.createProvider(
+            oauthConfig,
+            OauthSocialProvider.GOOGLE
+        );
+        return await googleProvider.verifyCode(responseCode);
+    } catch (error) {
+        throw error;
+    }
+}
 
+async function integrateOauthWithGoogleProvider() {
+    const responseCode = process.env.RESPONSE_CODE || '';
+    const userInfo = await getGoogleUserInfo(responseCode);
+    console.log({ userInfo });
 }
 
 (async () => {
-  await testOauthWithGoogle();
+    await integrateOauthWithGoogleProvider();
 })();
